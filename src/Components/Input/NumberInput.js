d9ed14311538de5c1e039290b12634f0df166eb2
Ari Key <zenzeleinbox@gmail.com>
2022-09-17 16:35:42 -0400

    Added NumberInput Component into app

---
+++ src/Components/Input/NumberInput.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './NumberInputStyle.css'
+
+const NumberInput = ({onChange, onKeyPress}) => {
+    return(
+        <div>
+            <input class="input-card-number"
+            type={'number'}
+            min= "1"
+            placeholder="Type in here"
+            onKeyPress={onKeyPress}
+            onChange={onChange}
+            />
+        </div>
+    )
+}
+
+export default NumberInput;
